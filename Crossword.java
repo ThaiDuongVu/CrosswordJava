96a4b1e1d75dcffe2df620bb4e3a71a1d132d0e2
Thai Duong Vu <vu.t.duong01@gmail.com>
2020-01-23 17:31:17 -0500

    Complete adding words to board

--- Crossword.java
+++ Crossword.java
@@ -1,75 +1,94 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Crossword {
-    private int boardSize = 20;
-    private String[] words = {"hello", "world", "my", "name", "is"};
-    private String blank = " ";
+    private int boardSize;
+    private ArrayList<String> words = new ArrayList<>();
 
-    private String[][] board = new String[boardSize][boardSize];
+    private ArrayList<String> unplacedWords = new ArrayList<>();
 
-    private ArrayList<String> placedWords = new ArrayList<String>();
-    private ArrayList<String> unplacedWords = new ArrayList<String>();
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         new Crossword().makeCrossword();
     }
 
     public void makeCrossword() {
+        System.out.println("How big do you want the grid? ");
+        boardSize = scanner.nextInt();
+
+        System.out.println("How many words to make crossword? ");
+        int numberOfWords = scanner.nextInt();
+
+        for (int i = 0; i < numberOfWords; i++) {
+            System.out.println("Enter word" + i + ": ");
+            String word = scanner.next();
+            words.add(word);
+        }
+
+        String[][] board = new String[boardSize][boardSize];
+
         for (int i = 0; i < boardSize; i++) {
             for (int j = 0; j < boardSize; j++) {
+                String blank = " ";
                 board[i][j] = blank;
             }
         }
-        printBoard();
+
+        addWords(board);
+        addUnplacedWords(board);
+        printBoard(board);
+
+        for (String word : unplacedWords) {
+            System.out.println("No matching found: " + word);
+        }
     }
 
-    private void printBoard() {
+    private void printBoard(String[][] board) {
         for (int y = -2; y < boardSize + 2; y++) {
             for (int x = -1; x < boardSize + 1; x++) {
                 if (y == -2 || y == boardSize + 1) {
                     if (x == -1 || x == boardSize) {
-                        System.out.print(" ");
+                        System.out.print(" " + " ");
                     }
                     else {
-                        System.out.print(x % 10);
+                        System.out.print(x % 10 + " ");
                     }
                 }
                 else if (y == -1 || y == boardSize) {
                     if (x == -1 || x == boardSize) {
-                        System.out.print(" ");
+                        System.out.print(" " + " ");
                     }
                     else {
-                        System.out.print("_");
+                        System.out.print("_" + " ");
                     }
                 }
                 else {
                     if (x == -1) {
-                        System.out.print("|");
+                        System.out.print("|" + " ");
                     }
                     else if (x == boardSize) {
                         System.out.print("|" + y);
                     }
                     else {
-                        System.out.print(board[x][y]);
+                        System.out.print(board[x][y] + " ");
                     }
                 }
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 
-    private void addFirstWord(String word) {
+    private void addFirstWord(String word, String[][] board) {
         int col = boardSize / 2 - word.length() / 2;
         int row = boardSize / 2;
 
         for (int x = col; x < word.length() + col; x++) {
             board[x][row] = Character.toString(word.charAt(x - col));
         }
-        placedWords.add(word);
     }
 
-    private boolean checkVertical(String word, int row, int col) {
+    private boolean checkVertical(String word, int row, int col, String[][] board) {
         int blankNumber = 0;
 
         if (word.length() + row > boardSize) {
@@ -87,7 +106,7 @@ public class Crossword {
             }
         }
 
-        for (int i = row; i < (word.length() + row); i++) {
+        for (int i = row; i < word.length() + row; i++) {
             if (i >= boardSize) {
                 return false;
             }
@@ -104,7 +123,7 @@ public class Crossword {
                         }
                     }
                     else {
-                        if (!board[col + 1][i].equals(" ") || !board[col - 1][i].equals(" ")) {
+                        if (!board[col + 1][i].equals(" ") || !board[col][i].equals(" ")) {
                             return false;
                         }
                         else {
@@ -122,23 +141,21 @@ public class Crossword {
         return blankNumber < word.length();
     }
 
-    private boolean addVertical(String word) {
+    private void addVertical(String word, String[][] board) {
         for (int col = 0; col < boardSize - 1; col++) {
             for (int row = 0; row < boardSize - word.length() + 1; row++) {
-                if (checkVertical(word, row, col)) {
+                if (checkVertical(word, row, col, board)) {
                     for (int i = row; i < word.length() + row; i++) {
                         board[col][i] = Character.toString(word.charAt(i - row));
                     }
-                    placedWords.add(word);
-                    return true;
+                    return;
                 }
             }
         }
         unplacedWords.add(word);
-        return false;
     }
 
-    private boolean checkHorizontal(String word, int row, int col) {
+    private boolean checkHorizontal(String word, int row, int col, String[][] board) {
         int blankNumber = 0;
 
         if (word.length() + col > boardSize) {
@@ -172,7 +189,7 @@ public class Crossword {
                         }
                     }
                     else {
-                        if (!board[i][row + 1].equals(" ") || !board[i][row - 1].equals(" ")) {
+                        if (!board[i][row + 1].equals(" ") || !board[i][row].equals(" ")) {
                             return false;
                         }
                         else {
@@ -190,32 +207,63 @@ public class Crossword {
         return blankNumber < word.length();
     }
 
-    private boolean addHorizontal(String word) {
+    private void addHorizontal(String word, String[][] board) {
         for (int row = 0; row < boardSize - 1; row++) {
             for (int col = 0; col < boardSize - word.length() + 1; col++) {
-                if (checkHorizontal(word, row, col)) {
+                if (checkHorizontal(word, row, col, board)) {
                     for (int i = col; i < word.length() + col; i++) {
                         board[i][row] = Character.toString(word.charAt(i - col));
                     }
-                    placedWords.add(word);
-                    return true;
+                    return;
                 }
             }
         }
         unplacedWords.add(word);
-        return false;
     }
 
-    private void addWords() {
-        for (int i = 0; i < words.length; i++) {
+    private void addWords(String[][] board) {
+        for (int i = 0; i < words.size(); i++) {
             if (i == 0) {
-                addFirstWord(words[i]);
+                addFirstWord(words.get(i), board);
             }
             if (i != 0 && i % 2 == 0) {
-                addHorizontal(words[i]);
+                addHorizontal(words.get(i), board);
             }
             if (i % 2 != 0) {
-                addVertical(words[i]);
+                addVertical(words.get(i), board);
+            }
+        }
+    }
+
+    private void addUnplacedWords(String[][] board) {
+
+        for (String word : unplacedWords) {
+            boolean test = true;
+
+            for (int row = 0; row < boardSize - word.length() + 1; row++) {
+                for (int col = 0; col < boardSize - 1; col++) {
+                    if (checkVertical(word, row, col, board) && test) {
+                        for (int i = row; i < word.length() + row; i++) {
+                            board[col][i] = Character.toString(word.charAt(i - row));
+                        }
+                        unplacedWords.remove(word);
+                        test = false;
+                        break;
+                    }
+                }
+            }
+
+            for (int row = 0; row < boardSize - 1; row++) {
+                for (int col = 0; col < boardSize - word.length() + 1; col++) {
+                    if (checkHorizontal(word, row, col, board) && test) {
+                        for (int i = col; i < word.length() + col; i++) {
+                            board[i][row] = Character.toString(word.charAt(i - col));
+                        }
+                        unplacedWords.remove(word);
+                        test = false;
+                        break;
+                    }
+                }
             }
         }
     }
